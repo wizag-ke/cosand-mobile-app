1046a787b68637c46f545f646a8453dbdbb2f8ea
pkavoo <pkavoo@gmail.com>
2018-12-18 10:39:21 +0300

    merge with master

--- app/src/main/java/wizag/com/supa/activity/Activity_Corporate_Client.java
+++ app/src/main/java/wizag/com/supa/activity/Activity_Corporate_Client.java
@@ -45,16 +45,15 @@ import wizag.com.supa.R;
 import wizag.com.supa.SessionManager;
 
 public class Activity_Corporate_Client extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
-    Button submit;
-    EditText office_location, cert_no, company_pin, company_name;
+    Button submit,upload_id_front, upload_id_back, upload_company_cert;
+    EditText company_name,registration_certificate_no,telephone_no,email_address, first_name, last_name, id_no, telephone_number;
     ViewFlipper flipper;
     String register_corporate_client_url = "http://sduka.wizag.biz/api/v1/profiles/roles";
-
-    String cert_no_txt,
-            company_pin_txt, company_name_txt;
+    ImageView id_front_image, id_back_image, company_cert_image;
+    String company_name_txt,registration_certificate_no_txt,telephone_no_txt,email_address_txt, first_name_txt, last_name_txt, id_no_txt, telephone_number_txt;
     EditText email;
-    LinearLayout image_layout;
-    ImageView id_image;
+    LinearLayout id_front,image_id_back,image_company_cert;
+    //ImageView id_image;
     private int SELECT_FILE = 1;
     private int REQUEST_CAMERA = 0;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
@@ -69,17 +68,31 @@ public class Activity_Corporate_Client extends AppCompatActivity implements View
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        office_location = findViewById(R.id.office_location);
-        cert_no = findViewById(R.id.cert_no);
-        company_pin = findViewById(R.id.company_pin);
+        id_front_image=findViewById(R.id.id_front_image);
+        id_back_image=findViewById(R.id.id_back_image);
+        company_cert_image=findViewById(R.id.company_cert_image);
+        upload_id_front=findViewById(R.id.upload_id_front);
+        upload_id_back=findViewById(R.id.upload_id_back);
+        upload_company_cert=findViewById(R.id.upload_company_cert);
         company_name = findViewById(R.id.company_name);
-        email = findViewById(R.id.email);
-        id_image = findViewById(R.id.id_image);
-        image_layout = findViewById(R.id.image_layout);
-        image_layout.setVisibility(View.GONE);
+        registration_certificate_no = findViewById(R.id.registration_certificate_no);
+        telephone_no = findViewById(R.id.telephone_no);
+        email_address = findViewById(R.id.email_address);
+        first_name=findViewById(R.id.first_name);
+        last_name=findViewById(R.id.last_name);
+        id_no=findViewById(R.id.id_no);
+        telephone_number=findViewById(R.id.telephone_number);
 
+
+
+
+       // email = findViewById(R.id.email);
+        id_front_image = findViewById(R.id.id_front_image);
+        id_back_image = findViewById(R.id.id_back_image);
+        company_cert_image = findViewById(R.id.company_cert_image);
+        id_front.setVisibility(View.GONE);
+        image_id_back.setVisibility(View.GONE);
+        image_company_cert.setVisibility(View.GONE);
         flipper = findViewById(R.id.flipper);
 
 
@@ -96,17 +109,26 @@ public class Activity_Corporate_Client extends AppCompatActivity implements View
 
             case R.id.submit:
 //                validate
-                if (office_location.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Enter Vehicle model to continue", Toast.LENGTH_LONG).show();
-                } else if (cert_no.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Enter Vehicle make to continue", Toast.LENGTH_LONG).show();
-                } else if (company_pin.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Enter Vehicle plate number to continue", Toast.LENGTH_LONG).show();
-                } else if (company_name.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Enter Vehicle logbook number to continue", Toast.LENGTH_LONG).show();
-                } else if (email.getText().toString().isEmpty()) {
-                    Toast.makeText(this, "Enter email to proceed", Toast.LENGTH_LONG).show();
-                } else if (!isNetworkConnected()) {
+                if (company_name.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Company Name to continue", Toast.LENGTH_LONG).show();
+                } else if (registration_certificate_no.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Registration Certificate No to continue", Toast.LENGTH_LONG).show();
+                } else if (telephone_no.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Telephone number to continue", Toast.LENGTH_LONG).show();
+                } else if (email_address.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Email Address to continue", Toast.LENGTH_LONG).show();
+                } else if (first_name.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter First Name to proceed", Toast.LENGTH_LONG).show();
+                }else if (last_name.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Last Name to proceed", Toast.LENGTH_LONG).show();
+                }
+                else if (id_no.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter ID Number to proceed", Toast.LENGTH_LONG).show();
+                }
+                else if (telephone_number.getText().toString().isEmpty()) {
+                    Toast.makeText(this, "Enter Telephone Number to proceed", Toast.LENGTH_LONG).show();
+                }
+                else if (!isNetworkConnected()) {
 
                     Toast.makeText(Activity_Corporate_Client.this, "Ensure you have internet connection", Toast.LENGTH_SHORT).show();
 
@@ -133,10 +155,14 @@ public class Activity_Corporate_Client extends AppCompatActivity implements View
         progressDialog.show();
         //getText
 
-        cert_no_txt = cert_no.getText().toString();
-        company_pin_txt = company_pin.getText().toString();
         company_name_txt = company_name.getText().toString();
-
+        registration_certificate_no_txt= registration_certificate_no.getText().toString();
+        telephone_no_txt=telephone_no.getText().toString();
+        email_address_txt=email_address.getText().toString();
+        first_name_txt=first_name.getText().toString();
+        last_name_txt=last_name.getText().toString();
+        id_no_txt=id_no.getText().toString();
+        telephone_number_txt=telephone_number.getText().toString();
         StringRequest stringRequest = new StringRequest(Request.Method.POST, register_corporate_client_url, new Response.Listener<String>() {
 
 
